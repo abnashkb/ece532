d87c77214611a35ec8625ae4135db8c74fec5f2d
Ahmed-Hamoda <61721040+Ahmed-Hamoda@users.noreply.github.com>
2024-01-23 08:21:19 -0500

    Update echoserver.py to allow continuous connection

--- warmup_demo_group/echoserver.py
+++ warmup_demo_group/echoserver.py
@@ -1,49 +1,52 @@
-""" Simple Server Example
+# Example of multi-client echo server
 
-This script creates a socket and listens for connections
-on any IPv4 interface on port 9090. When a connection is
-established, the server waits for a GET or POST command.
-
-GET:
-  FORMAT: b'GET'
-The server will send the current 32-bit SERVER_VALUE to the client.
-  
-POST:
-  FORMAT: b'POST' + XXXX
-The format of this command must be the four bytes corresponding
-to the ASCII characters 'POST' followed immediately by four
-bytes that contain the new value to set to SERVER_VALUE.
-"""
 import socket
+import _thread
 
-PORT = 22 #9090
+HOST = '192.168.1.11' # The IP address of the computer this script runs on (or change to 127.0.0.1 for localhost)
+PORT = 22 # The port used by this TCP server 
 BUFFER_SIZE = 1024
 SERVER_VALUE = bytes.fromhex('DEADBEEF')
 
-# Set up socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    # Allow re-binding the same port
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # Bind to port on any interface
-    sock.bind(('0.0.0.0', PORT))
-    sock.listen(1) # allow backlog of 1
+def open_new_client(connection, addr):
+    print("Connected to client ", addr[0], " port ", addr[1])
 
-    print("BEGIN LISTENING ON PORT", PORT)
-    # Begin listening for connections
-    while(True):
-       conn, addr = sock.accept()
-       with conn:
-           print("\nCONNECTION:", addr)
+    # Loop until connection closed
+    while True:
+        # Receive and handle command
+        data = connection.recv(BUFFER_SIZE)
+        if len(data) == 8 and data[:4] == b'POST':
+            SERVER_VALUE = data[4:8]
+            print("POST VALUE:", SERVER_VALUE.hex())
+        elif data == b'GET':
+            connection.send(SERVER_VALUE)
+            print("GET", SERVER_VALUE.hex())
+        else:
+            print("BAD COMMAND RECEIVED")
+            break
 
-           # Receive and handle command
-           data = conn.recv(BUFFER_SIZE)
-           if len(data) == 8 and data[:4] == b'POST':
-               SERVER_VALUE = data[4:8]
-               print("POST VALUE:", SERVER_VALUE.hex())
-           elif data == b'GET':
-              conn.send(SERVER_VALUE)
-              print("GET", SERVER_VALUE.hex())
-           else:
-               print("BAD COMMAND RECEIVED")
-           conn.shutdown(socket.SHUT_RDWR)
-           conn.close()
+    # Close the connection if break from loop
+    connection.shutdown(1)
+    connection.close()
+    print("Connection to client ", addr[0], " port ", addr[1], " closed")
+
+def listen():
+    # Setup the socket
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind to an address and port to listen on
+    connection.bind((HOST, PORT))
+    connection.listen(10)
+    print("Server opened on ",HOST," port ", PORT)
+
+    # Loop forever, accepting all connections in new thread
+    while True:
+        new_conn, new_addr = connection.accept()
+        _thread.start_new_thread(open_new_client,(new_conn, new_addr))
+
+if __name__ == "__main__":
+    try:
+        listen()
+    except KeyboardInterrupt:
+        pass
