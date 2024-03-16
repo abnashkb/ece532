027630fab1cbb90e37cfaa1b5c2dda11ac0f60a7
Martin Staadecker <machstg@gmail.com>
2024-03-16 12:20:21 -0400

    Update pivot column selection (Martin&Abnash)

--- lp_modules/find_pivot_col.v
+++ lp_modules/find_pivot_col.v
@@ -1,86 +1,69 @@
 `timescale 1ns / 1ps
 
-module find_pivot_col_new 
-	# (parameter DATAW = 32) //bit width of fifo, using single precision = 64 bits
-	(input clk,
-	input areset,
+module find_pivot_col
+	# (
+		parameter DATAW = 32, //bit width of fifo, using single precision = 64 bits
+		parameter COL_INDEX_SIZE = 16
+	) 
+	(
+		input clk,
+		input resetn, // This should connect to the FSM controller (as the start signal)
 	
-	//fsm inputs/outputs
-	input start,
-	output terminate,
-	output cont,
-	
-	//tableau size
-	input [15:0] num_cols,
-	
-	//axi signals for objective row data
-	input  [DATAW-1:0] axi_objrow_data,
-	input axi_objrow_valid,
-	output axi_objrow_ready,
-	
-	//lp algo outputs
-	output [15:0] pivot_col, //what width to use? depends on # of columns we have though. assume max 16384 = 2^14 so 14 bits --> round up to 16
-	output [DATAW-1:0] value_pivot_col_last_row
+		//fsm outputs
+		output reg terminate,
+		output reg cont,
+		
+		//tableau size
+		input [COL_INDEX_SIZE-1:0] num_cols,
+		
+		//axi signals for objective row data
+		input  [DATAW-1:0] axi_objrow_data,
+		input axi_objrow_valid,
+		output axi_objrow_ready,
+		
+		//lp algo outputs
+		output reg [COL_INDEX_SIZE-1:0] pivot_col, //what width to use? depends on # of columns we have though. assume max 16384 = 2^14 so 14 bits --> round up to 16
+		output reg [DATAW-1:0] value_pivot_col_last_row // TODO we can remove this
 	);
+
+	reg [COL_INDEX_SIZE-1:0] curr_col; //counter
 	
+	assign axi_objrow_ready = !terminate && !cont && resetn;
 	
-	reg [15:0] pivot_col_reg;
-	reg [15:0] curr_col_reg; //counter
-	reg [DATAW-1:0] value_pivot_col_last_row_reg;
-	reg found_pivot_col_reg;
-	//reg done_reg;
-	reg terminate_reg;
-	reg cont_reg;
-	reg axi_objrow_ready_reg;
-	assign axi_objrow_ready = axi_objrow_ready_reg;
-	
-	assign pivot_col = pivot_col_reg;
-	assign value_pivot_col_last_row = value_pivot_col_last_row_reg;
-	assign terminate = terminate_reg;
-	//assign done = done_reg;
-	assign cont = cont_reg;
-	
-	always @ (posedge clk, posedge areset) begin
-		if (areset) begin
-			axi_objrow_ready_reg <= 1'b0;
-		end
-		else begin
-			axi_objrow_ready_reg <= 1'b1; //can stay high, assuming one clk cycle for comparator logic below
-		end
-	end
-	
-	always @ (posedge clk or posedge areset)
+	always @ (posedge clk)
 	begin
-		if (areset) begin
-			pivot_col_reg <= 16'h0000;
-			curr_col_reg <= 16'h0000;
-			value_pivot_col_last_row_reg <= 32'h00000000; //most negative
-			terminate_reg <= 1'b0;
-			cont_reg <= 1'b0;
+		if (!resetn) begin
+			pivot_col <= 16'h0;
+			curr_col <= 16'h0;
+			value_pivot_col_last_row <= 32'h0; // Initialize as postive 0
+			terminate <= 1'b0;
+			cont <= 1'b0;
 		end
-		else begin
-			if (start && (curr_col_reg < num_cols) && axi_objrow_valid && axi_objrow_ready) begin
-				curr_col_reg <= curr_col_reg + 1;
-				found_pivot_col_reg <= 1'b0;
-				//done_reg <= 1'b0;
-				cont_reg <= 1'b0;
-				//if (axi_objrow_data < value_pivot_col_last_row_reg) begin
-				if ((axi_objrow_data[31] == 1'b1) && (axi_objrow_data[30:0] > value_pivot_col_last_row_reg[30:0])) begin
-					value_pivot_col_last_row_reg <= axi_objrow_data;
-					pivot_col_reg <= curr_col_reg + 1;
-				end
-			end
-			else if ((curr_col_reg == num_cols) && (value_pivot_col_last_row_reg < 0)) begin //streamed entire last row of tableau AND have negative number
-				terminate_reg <= 1'b0; //no error
-				cont_reg <= 1'b1; //continue onto next stage of LP algo, done with this module
+		else if (terminate || cont) begin end // Do nothing if we're already done
+		else if (curr_col == num_cols) begin
+			if (value_pivot_col_last_row[31]) begin // If we have a negative number, we've found a pivot column
+				cont <= 1'b1;
 			end
 			else begin
-				terminate_reg <= 1'b1; //did not find a valid pivot column out of entire last row
-				cont_reg <= 1'b0;
+				terminate <= 1'b1;
 			end
 		end
+		else begin
+		    
+			if (axi_objrow_valid && axi_objrow_ready) begin
+				curr_col <= curr_col + 1;
+				if (axi_objrow_data[30:23] == 8'b11111111) begin // It's infinity or NaN
+					terminate <= 1'b1; // An error occured!
+				end
+				// If value is smaller than our current value we update the pivot column
+				// We are doing a floating point number comparison
+				// Must be > not >= because we don't want to accept a -0 value.
+				else if ((axi_objrow_data[31] == 1'b1) && (axi_objrow_data[30:0] > value_pivot_col_last_row[30:0])) begin
+					value_pivot_col_last_row <= axi_objrow_data;
+					pivot_col <= curr_col;
+				end
+			end
+			// If the data isn't valid and ready, values are latched and nothing happens
+		end
 	end
-	
-	
-
 endmodule
--- lp_modules/find_pivot_col_tb.v
+++ lp_modules/find_pivot_col_tb.v
@@ -1,15 +1,14 @@
 `timescale 1ns / 1ps
 
-module find_pivot_col_new_tb ();
+module find_pivot_col_tb ();
 	parameter T = 10; //clk period
 	parameter DATAW = 32;
 	
 	reg clk_tb;
-	reg areset_tb;
+	reg resetn_tb;
 	
 	
 	//fsm inputs/outputs
-	reg start_tb;
 	wire terminate_tb;
 	wire cont_tb;
 	
@@ -24,33 +23,16 @@ module find_pivot_col_new_tb ();
 	//lp algo outputs
 	wire [15:0] pivot_col_tb; //what width to use? depends on # of columns we have though. assume max 16384 = 2^14 so 14 bits --> round up to 16
 	wire [DATAW-1:0] value_pivot_col_last_row_tb;
-	
-	
-	/*reg [DATAW-1:0] fifo_data_tb;
-	reg fifo_full_tb;
-	reg fifo_empty_tb;
-	wire [15:0] pivot_col_tb; //what width to use? depends on # of columns we have though. assume max 16384 = 2^14 so 14 bits --> round up to 16
-	wire [DATAW-1:0] value_pivot_col_last_row_tb;
-	wire found_pivot_col_tb;
-	wire done_tb;*/
 
-	/*find_pivot_col #(.FIFO_WIDTH(64)) inst0 //bit width of fifo, using double precision = 64 bits
-		(.clk(clk_tb),
-		.areset(areset_tb),
-		.fifo_data(fifo_data_tb), //element at front of fifo
-		.fifo_full(fifo_full_tb),
-		.fifo_empty(fifo_empty_tb),
-		.pivot_col(pivot_col_tb), //what width to use? depends on # of columns we have though. assume max 16384 = 2^14 so 14 bits --> round up to 16
-		.value_pivot_col_last_row(value_pivot_col_last_row_tb),
-		.found_pivot_col(found_pivot_col_tb),
-		.done(done_tb)
-		);*/
+    //clock
+	always begin
+		#(T/2) clk_tb = ~clk_tb; //so clock period is 20 time units
+	end
 		
-	find_pivot_col_new inst0 (.clk(clk_tb),
-	.areset(areset_tb),
+	find_pivot_col DUT (.clk(clk_tb),
+	.resetn(resetn_tb),
 	
 	//fsm inputs/outputs
-	.start(start_tb),
 	.terminate(terminate_tb),
 	.cont(cont_tb),
 	
@@ -67,37 +49,50 @@ module find_pivot_col_new_tb ();
 	.value_pivot_col_last_row(value_pivot_col_last_row_tb)
 	);
 	
-	initial begin
-		clk_tb = 1'b0;
-		areset_tb = 1'b1; //start high to reset at start of simulation to prevent x output from FFs (lack a default val)
-		axi_objrow_data_tb = 32'b0;
+//	function automatic void run_basic_test;
+    initial begin        
+        clk_tb = 1'b0;
+        resetn_tb = 1'b0; //start low to reset at start of simulation to prevent x output from FFs (lack a default val)
+        axi_objrow_data_tb = 32'b0;
         axi_objrow_valid_tb = 1'b0;
         num_cols_tb = 16'b0;
-        start_tb = 1'b0;
-		if (pivot_col_tb != 16'b0)
-			$error("ERROR: pivot_col_tb is not 0 during reset");
-		#(T*2); //wait 2 clk cycles
-		//bring reset back down
-		areset_tb = 1'b0;
-		num_cols_tb = 16'h02;
-		start_tb = 1'b1;
-		#(T*2); //wait 2 clk cycles
+        if (pivot_col_tb != 16'b0)
+            $error("ERROR: pivot_col_tb is not 0 during reset");
+        #(T*2); //wait 2 clk cycles
+        //bring reset back down
+        resetn_tb = 1'b1;
+        num_cols_tb = 16'h05;
+        #(T*2); //wait 2 clk cycles
         @(posedge clk_tb); //wait for rising edge of clk
-		//test 1 - provide negative value on fifo_data_tb
-		axi_objrow_valid_tb = 1'b1;// = 1'b0; //fifo is not empty
-		axi_objrow_data_tb = 32'b11000001001000000000000000000000; //-10//-64'h00000000000000ff; //
-		#T;
-		//test 2 - provide positive value on fifo_data_tb
-		axi_objrow_data_tb = 32'b01000000100000000000000000000000; //4
-		#T;
-		axi_objrow_valid_tb = 1'b0; //fifo is empty	
-		#(T*2);
-	$finish;
-	end
+        //test 1 - provide positive value on fifo_data_tb
+        axi_objrow_data_tb = 32'b01000000100000000000000000000000; //4
+        #T;
+        
+        //test 2 - provide negative value on fifo_data_tb
+        axi_objrow_valid_tb = 1'b1;// = 1'b0; //fifo is not empty
+        axi_objrow_data_tb = 32'b11000001001000000000000000000000; //-10//-64'h00000000000000ff; //
+        #T;
+        
+        //test 3 - provide negative 0
+        axi_objrow_data_tb = 32'h80000000; 
+        #T;
+        
+        
+        //test 4 - provide - INF
+        axi_objrow_data_tb = 32'hff800000;
+        #T;
+        
+        axi_objrow_valid_tb = 1'b0; //fifo is empty	
+        #(T*2);
+        $finish;  
+    end
 	
-	//clock
-	always begin
-		#(T/2) clk_tb = ~clk_tb; //so clock period is 20 time units
-	end
-		
+
+//	function automatic void main;
+//	   run_basic_test();
+//	endfunction;
+	
+//	initial
+//	   main();
+//	begin
 endmodule
